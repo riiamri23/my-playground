c155895c3c4c3580f41ce9551e1056d3896d5f21
riiamri <riiamri23@gmail.com>
2023-06-30 14:57:48 +0700

    adding carouselslider

--- src/router/RouterApp.js
+++ src/router/RouterApp.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from '../pages/home/Index';
 import TodoFirebase from '../pages/todofirebase/Index';
+import CarouselSlider from '../pages/carouselslider';
 
 
 export default function RouterApp() {
@@ -12,6 +13,7 @@ export default function RouterApp() {
             <Routes>
                 <Route index element={<Home />} />
                 <Route path="todofirebase" element={<TodoFirebase />} />
+                <Route path="carouselslider" element={<CarouselSlider />} />
             </Routes>
         </Router>);
 }
